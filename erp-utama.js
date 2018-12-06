d5cbca0d42521d23ffb0e77f35e1aaab493d9450
riyad12 <riyadhidayat1993@gmail.com>
2018-12-07 06:14:22 +0700

    Update erp-utama.js

--- erp-utama.js
+++ erp-utama.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/09/top-five-things-to-do-prior-to.html"}
 if(x=="2"){xxx="/2018/09/your-erp-implementation-best-practices.html"}
 if(x=="3"){xxx="/2018/09/five-erp-software-trends-to-watch-in.html"}
 if(x=="4"){xxx="/2018/09/five-surprising-takeaways-from-2018-erp.html"}
-if(x=="5"){xxx="/2018/09/top-predictions-for-erp-software.html"}a_to_ve[i].href="http://www.onicnetwork.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/09/top-predictions-for-erp-software.html"}a_to_ve[i].href="http://www.reccanetwork.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
